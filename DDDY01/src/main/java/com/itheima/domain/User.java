c3ef2e23331bd5d3c6bb316bd5d7eb341f711c15
DDDY <2178993102@qq.com>
2020-01-15 21:06:21 +0800

    第三次上传

--- DDDY01/src/main/java/com/itheima/domain/User.java
+++ DDDY01/src/main/java/com/itheima/domain/User.java
@@ -5,4 +5,11 @@ public class User {
     private Integer age;
     private String address;
     private String name2;
+
+    public User(String name, Integer age, String address, String name2) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.name2 = name2;
+    }
 }
